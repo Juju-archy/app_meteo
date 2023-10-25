1fee93b92b01e92100aeab417d33fe377497663d
BuildTools <unconfigured@null.spigotmc.org>
2023-10-25 22:40:47 +0200

    Avancement projet appli météo

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,8 +34,15 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  List<String> cities = ["Paris", "Toulouse", "Marseilles"];
+  String key = 'villes';
+  late List<String> cities = [];
   String citySelected = '';
+
+  @override
+  void initState() {
+    super.initState();
+    getCity();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,7 +58,19 @@ class _MyHomePageState extends State<MyHomePage> {
             itemCount: cities.length + 2,
             itemBuilder: (context, i){
               if (i == 0) {
-                return const DrawerHeader(child: Text('test'));
+                return DrawerHeader(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      textWithStyle("Mes villes", fontSize: 22.0),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(backgroundColor: Colors.white, elevation: 10.0),
+                        onPressed: addCity,
+                        child: textWithStyle("Ajouter une ville", color: Colors.deepPurple),
+                      ),
+                    ],
+                  ),
+                );
               } else if(i == 1) {
                 return ListTile(
                   title: textWithStyle("Ma ville actuelle"),
@@ -92,5 +113,44 @@ class _MyHomePageState extends State<MyHomePage> {
         fontSize: fontSize,
       ),
     );
+  }
+
+  Future<void> addCity() async {
+    return showDialog(
+      barrierDismissible: true,
+      context: context,
+      builder: (BuildContext buildcontext) {
+        return SimpleDialog(
+          contentPadding: EdgeInsets.all(20.0),
+          title: textWithStyle("Ajoutez une ville", fontSize: 22.0, color: Colors.deepPurple),
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: "ville: "),
+              onSubmitted: (String str){
+                ajouter(str);
+                Navigator.pop(buildcontext);
+              },
+            )
+          ],
+        );
+      },
+    );
+  }
+
+  void getCity() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    List<String>? list = await sharedPreferences.getStringList(key);
+    if(list != null){
+      setState(() {
+        cities = list;
+      });
+    }
+  }
+
+  void ajouter(String str) async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    cities.add(str);
+    await sharedPreferences.setStringList(key, cities);
+    getCity();
   }
 }
